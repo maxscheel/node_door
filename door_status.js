eeeed4f13385d54de3463c03ac0f51e8eb3308c4
Marcus <best.nose@gmail.com>
2016-08-19 16:52:35 +1200

    initial fine for module for status of door

---
+++ door_status.js
@@ -0,0 +1,18 @@
+// not sure about the "submit" bit
+module.exports.submit = doorStatus
+
+function doorStatus() {
+  var open = 
+  var closed = 
+  if {
+    open == 1 | closed == 0
+    status = open
+  }
+  else {
+   open == 0 | closed == 1
+   status = closed
+  }
+  else {
+    status = unknown
+  }
+}
